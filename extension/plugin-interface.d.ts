9bcf1a4e613a4568586421f6783554f669d53c76
mikob <492864+mikob@users.noreply.github.com>
2022-06-06 16:47:38 -0500

    jesus christ, Google Docs. The obfuscation is mocking us.

--- extension/plugin-interface.d.ts
+++ extension/plugin-interface.d.ts
@@ -217,8 +217,8 @@ declare interface IPluginUtil {
 
   getCmdHistory: () => [pluginId: string, cmdName: string, cmdArgs: CmdArgs][];
 
-  connectToPage: (frontendFn: () => void) => IPageConnection;
-  timeout: <T>(p: Promise<T>, time: number) => Promise<T | void>;
+  connectToPage: (frontendFn: () => function) => IPageConnection;
+  timeout: <T>(p: Promise<T>, time: number) => Promise<T>;
 
   // takes into account LipSurf dialogues that are in the shadow DOM (eg. custom homosyn adder)
   getActiveEl: () => HTMLElement;
